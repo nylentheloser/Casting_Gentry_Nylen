334e0569712e04a643d3f4be7d372cfc5719c3d0
nylentheloser <nylengentry08@gmail.com>
2025-02-27 10:13:09 -0600

    Initial commit

---
+++ Casting_Gentry_Nylen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Casting_Gentry_Nylen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Casting_Gentry_Nylen
+ 
